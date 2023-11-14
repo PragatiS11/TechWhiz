dfe0c0246e860beec8aa52832db39b441e920f26
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2023-11-14 05:30:47 +0530

    fw26_268

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -3,11 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 // import Generate from './component/Generate';
 import Chatbot from './component/Generate';
+import Dashboard from './component/Dashboard';
 
 function App() {
   return (
     <div className="App">
-       <Chatbot/>
+       {/* <Chatbot/> */}
+
+   <Dashboard/>
+
     </div>
   );
 }
